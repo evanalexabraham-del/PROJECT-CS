9813befef44e2e373144070166fa7653540f349b
yuh <sidmc060209@gmail.com>
2026-09-06 21:46:36 +0530

    Update PROJECT CS 12-G1.py

--- PROJECT CS 12-G1.py
+++ PROJECT CS 12-G1.py
@@ -26,9 +26,12 @@ def ADDAIRCRAFT():
     print("RECORD ADDED!")
 def VIEWAIRCRAFT():
     print("=============AIRCRAFT DETAILS===============")
-    mycursor.execute("SELECT * FROM aircraft;")
-    for i in mycursor:
-        print (i)
+    data= mycursor.fetchall()
+    print("AIRCRAFT ID\tAIRLINE\tAIRCRAFT TYPE\tSTATUS")
+    for i in data:
+        print(i[0]"\t" i[1] "\t"i[2] "\t"i[3])
+    print("============================================")
+    
 
 def SEARCHAIRCRAFT():
     ID = int(input("Enter Aircraft ID to search: "))
