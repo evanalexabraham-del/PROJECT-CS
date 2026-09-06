c119d5685f9ff505b809de92bc091adb1a254517
yuh <sidmc060209@gmail.com>
2026-09-06 18:31:47 +0530

    Update PROJECT CS 12-G1.py

--- PROJECT CS 12-G1.py
+++ PROJECT CS 12-G1.py
@@ -13,15 +13,17 @@ mycursor = mydb.cursor()
 
 #AIRCRAFT RELATED FUNCTION
 def ADDAIRCRAFT():
-    aid=int(input("Enter 3 digit aircraft ID: "))
+    aid = int(input("Enter 3 digit aircraft ID: "))
     airline = input("Enter airline: ")
-    atype = input("Enter Aircraft type:")
-    stat=input("Enter status of aircraft:")
-    mycursor.execute("INSERT INTO aircraft VALUES(aid, airline,atype,stat;)
+    atype = input("Enter Aircraft type: ")
+    stat = input("Enter status of aircraft: ")
+
+    mycursor.execute(
+        "INSERT INTO aircraft VALUES (%s, %s, %s, %s)",
+        (aid, airline, atype, stat)
+    )
     mydb.commit()
     print("RECORD ADDED!")
-
-
 def VIEWAIRCRAFT():
     print("=============AIRCRAFT DETAILS===============")
     mycursor.execute("SELECT * FROM aircraft;")
@@ -29,14 +31,21 @@ def VIEWAIRCRAFT():
         print (i)
 
 def SEARCHAIRCRAFT():
-    ID= int(input("Enter Aircraft ID to search"))
+    ID = int(input("Enter Aircraft ID to search: "))
     print("DETAILS OF AIRCRAFT SEARCHED:")
-    mycursor.execute("SELECT * FROM aircraft WHERE aircraftID = ID ;")
+
+    mycursor.execute("SELECT * FROM aircraft WHERE aircraftID = %s", (ID,))
+    res = mycursor.fetchone()
+
+    if result:
+        print(res)
+    else:
+        print("AIRCRAFT NOT FOUND!")
 
 def UPDATEAIRCRAFT():
     ID = int(input("Enter ID of aircraft whose status is to be updated:"))
     newstat = input("Enter new status:")
-    mycursor.execute("UPDATE aircraft SET status = '"+newstat+"' WHERE aircraftID = '"+ID+"';")
+    mycursor.execute("UPDATE aircraft SET status = %s WHERE aircraftID = %s;", (newstat, ID))
     mydb.commit()
     print("UPDATED TABLE:")
     mycursor.execute("SELECT * FROM aircraft;")
